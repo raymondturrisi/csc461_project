209ebd5061494de770f17c984ad88aec43ce09b2
Alfred Timperley <atimperley15@gmail.com>
2021-12-10 01:44:27 -0500

    updated models

--- compiler_gym/experiments/train_model.py
+++ compiler_gym/experiments/train_model.py
@@ -4,6 +4,8 @@ import calendar
 import time
 
 from stable_baselines3 import DQN
+from compiler_gym.wrappers import TimeLimit
+from compiler_gym.wrappers import RandomOrderBenchmarks
 
 def train_env():
     env = compiler_gym.make(
@@ -11,6 +13,15 @@ def train_env():
         reward_space="IrInstructionCountOz",
         observation_space="InstCountNorm"
     )
+
+        
+    del env.datasets["cbench-v1"]
+    del env.datasets["generator://csmith-v0"]
+    del env.datasets["generator://llvm-stress-v0"]
+    train_benchmarks = env.datasets
+
+    env = TimeLimit(env, max_episode_steps=2000)
+    env = RandomOrderBenchmarks(env, list(train_benchmarks))
     return env
 
 def test_env():
@@ -25,43 +36,20 @@ def test_env():
 
 
 def train(env, model):
-    
-    del env.datasets["cbench-v1"]
-    del env.datasets["generator://csmith-v0"]
-    del env.datasets["generator://llvm-stress-v0"]
-    train_benchmarks = env.datasets
-    episodes = 50000 # The number of episodes used to learn
-    episode_length = 12 # The maximum number of transformations
-    patience = 6 # The maximum transformations with zero change tolerated before a new episode
-    for i in range(1, episodes+1):
-        obs = env.reset(benchmark = train_benchmarks.random_benchmark())
-        done = False
-        action_count = 0
-        actions_since_last_change = 0
-        episode_reward = 0
 
-        while not done and action_count < episode_length and actions_since_last_change < patience: 
-            # action, _ = model.predict(obs)
-            action = env.action_space.sample()
-            new_obs, reward, done, info = env.step(action)
-            action_count += 1
-            episode_reward += reward
-
-            if reward == 0:
-                actions_since_last_change += 1
-            else:
-                actions_since_last_change = 0
-            
-            model.learn(total_timesteps=1)
-
-            obs = new_obs
-
-            # print("Step: " + str(i) + " Episode Total: " + "{:.4f}".format(episode_reward) + " Action: " + str(action))
+    episodes = 1000000 # The number of episodes used to learn
+    episode_length = 5 # The maximum number of transformations
+    patience = 30 # The maximum transformations with zero change tolerated before a new episode
+    for i in range(1, episodes+1):  
+        model.learn(total_timesteps=episode_length)
         
-        print ("Average Reward for Step " + str(i) + ": " + str(episode_reward/episode_length) + "\nTotal Reward: " + str(episode_reward))
-        ts = calendar.timegm(time.gmtime())
-        if i % 1000 == 0:
-            model.save("models/DQN_model_"+str(ts))
+        if i % 100 == 0:
+            print ("Step: " + str(i))
+        if i % 5000 == 0:
+            ts = calendar.timegm(time.gmtime())
+            model.save("models/DQN_model_1"+str(ts))
+            model.predict()
+
     
     print("DONE")
 
