b77ef7fddb2ee9283c25f137093642cb72139395
Alfred Timperley <atimperley15@gmail.com>
2021-12-14 17:12:55 -0500

    fixes

--- compiler_gym/experiments/train_watchdog.sh
+++ compiler_gym/experiments/train_watchdog.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for COUNT in {1..2200}
 do
-    python3 train_model_DQN.py $COUNT $2
+    python3 train_model_DQN.py $COUNT $1
     COUNT=$(( $COUNT + 1 ))
     echo "One loop done, Count is now at $COUNT"
 done
