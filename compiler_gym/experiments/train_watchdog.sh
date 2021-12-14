e1c0f632eefc1575e2043c78cfa6ecf9960dc01b
Alfred Timperley <atimperley15@gmail.com>
2021-12-14 15:18:50 -0500

    hope this works

--- compiler_gym/experiments/train_watchdog.sh
+++ compiler_gym/experiments/train_watchdog.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-if [[ $1 == "DQN"]]
-then
+if [[ $1 == "DQN"]]; then
     for COUNT in {1..2200}
     do
         python3 train_model_DQN.py $COUNT $2
         COUNT=$(( $COUNT + 1 ))
         echo "One loop done, Count is now at $COUNT"
     done
-fi
 
-if [[ $1 == "A2C"]]
-then
+elif [[ $1 == "A2C"]]; then
     for COUNT in {1..2200}
     do
         python3 train_model_A2C.py $COUNT $2
