eb8320c9c863f588042f843df2c7423531e42afc
Alfred Timperley <atimperley15@gmail.com>
2021-12-14 14:52:57 -0500

    added ppo and working on dqn and a2c

--- compiler_gym/experiments/model_watchdog.sh
+++ compiler_gym/experiments/model_watchdog.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 COUNT=1
-while :
-do
-	python3 train_model.py $COUNT $1
-    COUNT = COUNT + 1
-done
+if [ $1 = "DQN"]
+then
+    while :
+    do
+        python3 train_model_DQN.py $COUNT $2
+        COUNT = COUNT + 1
+    done
+fi
+
+if [ $1 = "A2C"]
+then
+    while :
+    do
+        python3 train_model_A2C.py $COUNT $2
+        COUNT = COUNT + 1
+    done
+fi
