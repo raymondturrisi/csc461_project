ae51bd6e97b36ebfe3eb6510d91aa32b4c498e5f
Alfred Timperley <atimperley15@gmail.com>
2021-12-14 17:11:57 -0500

    fixes

--- compiler_gym/experiments/train_watchdog _a2c.sh
+++ compiler_gym/experiments/train_watchdog _a2c.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for COUNT in {1..2200}
 do
-    python3 train_model_DQN.py $COUNT $2
+    python3 train_model_A2C.py $COUNT $2
     COUNT=$(( $COUNT + 1 ))
     echo "One loop done, Count is now at $COUNT"
 done
